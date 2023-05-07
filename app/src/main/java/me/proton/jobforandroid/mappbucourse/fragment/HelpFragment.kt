2f0d02cc1aaea6df393fd9df550d280072ca254d
Galyaminsky_Des <jobforandroid@proton.me>
2023-05-07 15:33:27 +0300

    TopAppBar - Completed

---
+++ app/src/main/java/me/proton/jobforandroid/mappbucourse/fragment/HelpFragment.kt
@@ -0,0 +1,25 @@
+package me.proton.jobforandroid.mappbucourse.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.proton.jobforandroid.mappbucourse.R
+
+class HelpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_help, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = HelpFragment()
+    }
+}

671004cf4a429e5034bc4f16479708f64374d9e5
Galyaminsky_Des <jobforandroid@proton.me>
2023-05-07 16:13:42 +0300

    BottomAppBar - Completed

--- app/src/main/java/me/proton/jobforandroid/mappbucourse/MainActivity.kt
+++ app/src/main/java/me/proton/jobforandroid/mappbucourse/MainActivity.kt
@@ -1,8 +1,14 @@
 package me.proton.jobforandroid.mappbucourse
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import me.proton.jobforandroid.mappbucourse.databinding.ActivityMainBinding
+import me.proton.jobforandroid.mappbucourse.fragment.CatalogFragment
+import me.proton.jobforandroid.mappbucourse.fragment.DeliveryFragment
+import me.proton.jobforandroid.mappbucourse.fragment.HomeFragment
+import me.proton.jobforandroid.mappbucourse.fragment.SettingFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +19,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.bottomBar)
+
+        binding.bottomBar.setOnMenuItemClickListener { menuItem -> itemSelected(menuItem) }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.bottom_nav, menu)
+        return true
+    }
+
+    private fun itemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.contener, HomeFragment.newInstance()).commit()
+
+            R.id.item_catalog -> supportFragmentManager.beginTransaction()
+                .replace(R.id.contener, CatalogFragment.newInstance()).commit()
+
+            R.id.item_delivery -> supportFragmentManager.beginTransaction()
+                .replace(R.id.contener, DeliveryFragment.newInstance()).commit()
+
+            R.id.item_setting -> supportFragmentManager.beginTransaction()
+                .replace(R.id.contener, SettingFragment.newInstance()).commit()
+        }
+        return true
     }
 }

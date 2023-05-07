cd20699856209cea12853fc23a835328e2f20c95
Galyaminsky_Des <jobforandroid@proton.me>
2023-05-07 16:51:35 +0300

    Floating_Action_Button_and_Extented #2

--- app/src/main/java/me/proton/jobforandroid/mappbucourse/MainActivity.kt
+++ app/src/main/java/me/proton/jobforandroid/mappbucourse/MainActivity.kt
@@ -1,10 +1,12 @@
 package me.proton.jobforandroid.mappbucourse
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import me.proton.jobforandroid.mappbucourse.databinding.ActivityMainBinding
+import me.proton.jobforandroid.mappbucourse.fragment.AlcoFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,7 +15,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //binding.fab.setColorFilter(Color.CYAN)
+        binding.fab.hide()
 
+        binding.fab.setOnClickListener(this)
+        binding.btnDis.setOnClickListener(this)
+        binding.btnHide.setOnClickListener(this)
     }
 
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.fab -> supportFragmentManager.beginTransaction().replace(R.id.content, AlcoFragment.newInstance())
+                .commit()
+            R.id.btnDis -> binding.fab.show()
+            R.id.btnHide -> binding.fab.hide()
+        }
+    }
 }
